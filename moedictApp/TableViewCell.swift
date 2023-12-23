4909ecb9bd9a891cbef1e28341ca989b5a914fc9
Min Hu <lebonthe@gmail.com>
2023-12-24 02:46:21 +0800

    first

---
+++ moedictApp/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  moedictApp
+//
+//  Created by Min Hu on 2023/12/22.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ moedictApp/WordDefinition.swift
@@ -0,0 +1,8 @@
+//
+//  WordDefinition.swift
+//  moedictApp
+//
+//  Created by Min Hu on 2023/12/22.
+//
+
+import Foundation
